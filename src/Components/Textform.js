9cb69735be7009712a412598d1052e880b3a737e
Anupam Kumar <101698944+anupamjhabbl@users.noreply.github.com>
2022-06-16 12:40:08 +0530

    solve word count bug,add copy button,button disable function added,shifting problem due to alert solved

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -22,6 +22,7 @@ function Textform(props){
     
 
     const Uppercase=()=>{
+        
         setText(text.toUpperCase());
         props.showAlert("You are changing into uppercaes","Success");
         
@@ -32,6 +33,11 @@ function Textform(props){
         props.showAlert("You are changing into lowercase","Success");
         
     }
+    const copy=()=>{
+        navigator.clipboard.writeText(text);
+        props.showAlert("Item copied","Success");
+        
+    }
 
     const handleOnChange=(event)=>{
         //console.log("on change");
@@ -39,9 +45,18 @@ function Textform(props){
         //props.showAlert("You are changing input value","Success");
         
     }
+
+    
     
 
     const [text,setText]= useState('');
+
+    
+
+    
+
+    
+
     
     return(
         <>
@@ -50,12 +65,21 @@ function Textform(props){
                 <label htmlFor="exampleFormControlTextarea1" className={`form-label text-${modevalue}`}>Textbox</label>
                 <textarea className="form-control" id="exampleFormControlTextarea1" rows="10"  value={text} onChange={handleOnChange}  style={props.mystyle}></textarea>
                 
-                <button type="button" className="btn btn-success btnt" id="Uppercase"onClick={Uppercase}>Uppercase</button>
-                <button type="button" className="btn btn-danger btnt" id="Lowercase" onClick={Lowercase}>Lowercase</button>
+                <button disabled={text.length==0} type="button" className="btn btn-success btnt" id="Uppercase"onClick={Uppercase}>Uppercase</button>
+                <button disabled={text.length==0} type="button" className="btn btn-danger btnt" id="Lowercase" onClick={Lowercase}>Lowercase</button>
+                <button disabled={text.length==0} type="button" className="btn btn-primary btnt" id="Lowercase" onClick={copy}>Copy</button>
 
                 <div className={`text-${modevalue} mb2`}>
                     <h2>Your Text Summary</h2>
-                    <p>{!(text.length) || text[text.length-1]===' ' ? (text.split(" ").length)-1:text.split(" ").length} words and {text.length} characters time needed {((text.split(" ").length)/300)*60} seconds.</p>
+                    <p>{text.split(/\s+/).filter((element)=>{
+                        if (element!=" " && element!="\n"){
+                            return element;
+                        }
+                    }).length} words and {text.length} characters time needed {((text.split(/\s+/).filter((element)=>{
+                        if (element!=" " && element!="\n"){
+                            return element;
+                        }
+                    }).length)/300)*60} seconds.</p>  {/*text.split(/\s+/) will give give */}
                 </div>
 
                 <h2 className={` text-${modevalue}`}>Preview</h2>
@@ -71,3 +95,5 @@ function Textform(props){
 }
 
 export default Textform;
+
+
